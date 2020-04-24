0d84df3441417a94c9a81393d58b77cb62d480bc
swapnilpatil <swapnilpatil242@gmail.com>
2020-04-24 18:17:08 +0530

    updated class and inheritance files

--- class.php
+++ class.php
@@ -1,5 +1,6 @@
 <?php
 class Person {
+  // Properties
   public $firstName;
   protected $lastName;
   private $gender;
@@ -11,12 +12,19 @@ class Person {
     $this->gender = $gender;
   }
 
+  function __destruct() {
+    echo "destruct()...\n";
+    echo "This value for firstName is: {$this->firstName}.\n";
+  }
+
+  // Methods
   public function displayPersion(){
     echo "displayPersion()..:: ".$this->firstName." ".$this->lastName." ".$this->gender;
   }
 }
 
 $obj1 = new Person("Swapnil", "Patil", "Male");
+// echo var_dump($obj1 instanceof Person); // return is it object of that class or not
 echo "obj accessing public member:: ".$obj1->firstName;
 echo "\n";
 $obj1->displayPersion();
@@ -26,3 +34,9 @@ echo "\n";
 echo "\n";
 // echo "obj accessing private member:: ".$obj1->gender;
 // o/p: PHP Fatal error:  Uncaught Error: Cannot access private property Person::$gender
+
+
+
+// public - the property or method can be accessed from everywhere. This is default
+// protected - the property or method can be accessed within the class and by classes derived from that class
+// private - the property or method can ONLY be accessed within the class
--- inheritance.php
+++ inheritance.php
@@ -70,8 +70,44 @@ echo "\n";
 
 
 # Inheritance ::
+  // PHP only supports single inheritance: a child class can inherit only from one single parent.
   // used key word "extends" to inherit all parent properties
   // it inherits all of the public and protected methods as well as properties from Parent class
   // Private: Access within class only, Does not outside class
   // Protected :: Access in class and inherited class, Does not outside in those class
   // public :: Access in class and putside of class as well
+
+//   - The final keyword can be used to prevent class inheritance or to prevent method overriding.
+//   The following example shows how to prevent class inheritance:
+//   final class Fruit {
+//     // some code
+//   }
+// - The following example shows how to prevent method overriding:
+//   final public function intro() {
+//     // some code
+//   }
+
+
+# Traits ::
+// So, what if a class needs to inherit multiple behaviors? OOP traits solve this problem.
+
+// Traits are used to declare methods that can be used in multiple classes. Traits can have methods and abstract methods that can be used in multiple classes, and the methods can have any access modifier (public, private, or protected).
+// - You can use Multiple Traits
+// - To use a trait in a class, use the use keyword:
+// <?php
+//   trait message1 {
+//   public function msg1() {
+//       echo "OOP is fun! ";
+//     }
+//   }
+
+//   class Welcome {
+//     use message1;
+//   }
+
+//   $obj = new Welcome();
+//   $obj->msg1();
+// ?>
+// Here, we declare one trait: message1. Then, we create a class: Welcome. The class uses the trait, and all the methods in the trait will be available in the class.
+
+// If other classes need to use the msg1() function, simply use the message1 trait in those classes. This reduces code duplication, because there is no need to redeclare the same method over and over again.
